b3fdcde67dd90876c7e3572fa163e78e518a27db
Eric manishimwe <manishimweeric54@gmail.com>
2024-12-19 12:00:53 +0200

    update

--- src/components/admin/AdminLogin.js
+++ src/components/admin/AdminLogin.js
@@ -13,7 +13,7 @@ const AdminLogin = () => {
     setSuccess("");
   
     try {
-      const response = await fetch("http://localhost:8081/api/admin/login", {
+      const response = await fetch("https://backendauca-9b41fc378333.herokuapp.com/api/admin/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
